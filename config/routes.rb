a1c4cb70b7d183449f8698f0858bf5f11aa4b7d6
saya-gg <slamdunk31117@yahoo.co.jp>
2019-06-28 15:59:49 +0000

    user modify controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   root to: 'tasks#index'
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :tasks
+  resources :tasks, :users
 end
